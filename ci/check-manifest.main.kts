e1909882fc4c9096133e507eea036be795d117d3
Hiroshi Miura <miurahr@linux.com>
2023-05-03 10:36:21 +0900

    feat: Support multiple versions added
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- ci/check-manifest.main.kts
+++ ci/check-manifest.main.kts
@@ -3,14 +3,25 @@
 
 import java.util.jar.JarFile
 
+// Mapping from Jar attributes to plugin attribute keys
 val terms = mapOf("Plugin-Name" to "Name", "Bundle-Name" to "Name", "Implementation-Title" to "Name",
         "Plugin-Version" to "Version", "Bundle-Version" to "Version", "Implementation-Version" to "Version",
         "Plugin-Author" to "Author", "Implementation-Vendor" to "Author", "Built-By" to "Author",
-        "Plugin-Description" to "Description", "Plugin-Link" to "Link", "Plugin-Category" to "Category")
-val checkList = mutableMapOf("Name" to false, "Version" to false, "Author" to false,
-        "Description" to false, "Link" to false, "Category" to false)
+        "Plugin-Category" to "Category",
+        "Plugin-Description" to "Description", "Plugin-Link" to "Link",
+)
 
-var targetJarFile = args.toList().first()
+// These are mandatory attributes
+val checkList = mutableMapOf("Name" to false, "Version" to false, "Author" to false, "Category" to false)
+
+// Accept only one argument as jar file
+if (args.size != 1) {
+    System.err.println("Multiple jar files seems to be added...")
+    System.err.println("It should be exactly one plugin jar file to add.")
+    System.exit(1)
+}
+
+var targetJarFile = args[0]
 val input = JarFile(targetJarFile)
 val mainAttr = input.manifest.mainAttributes
 for (entry in mainAttr) {
