d3f26f061e4455ea19ecadbc90250db861ee537f
Hiroshi Miura <miurahr@linux.com>
2023-08-08 08:12:02 +0900

    chore: check category value and show it in actions summary
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- ci/check-manifest.main.kts
+++ ci/check-manifest.main.kts
@@ -1,6 +1,7 @@
 @file:JvmName("CheckManifest")
 @file:CompilerOptions("-jvm-target", "11")
 
+import java.util.jar.Attributes
 import java.util.jar.JarFile
 
 // Mapping from Jar attributes to plugin attribute keys
@@ -14,6 +15,23 @@ val terms = mapOf("Plugin-Name" to "Name", "Bundle-Name" to "Name", "Implementat
 // These are mandatory attributes
 val checkList = mutableMapOf("Name" to false, "Version" to false, "Author" to false, "Category" to false,
         "Class-Name" to false)
+
+// Check acceptable category values
+fun getAttr(attr: Attributes, term: String): String {
+    for (entry in attr) {
+        when (terms.get(entry.key.toString())) {
+            term -> return entry.value.toString()
+        }
+    }
+    return "unknown"
+}
+
+fun getCategory(attr: Attributes): String {
+    return getAttr(attr, "Category")
+}
+
+val categoryList = mutableListOf("filter", "tokenizer", "marker", "machinetranslator", "base", "glossary", "dictionary",
+        "theme", "miscellaneous")
 
 // Accept only one argument as jar file
 if (args.size != 1) {
@@ -33,3 +51,16 @@ if (result != null) {
     System.err.println(String.format("%s is missing in MANIFEST.MF", result))
     System.exit(1)
 }
+
+val category = getCategory(mainAttr)
+if (category !in categoryList) {
+    System.err.println(String.format("Category %s is invalid value in MANIFEST.MF", category))
+    System.exit(1)
+}
+
+// print manifest summary
+System.out.println("Manifest summary")
+System.out.println(String.format("Name: %s", getAttr(mainAttr, "Name")))
+System.out.println(String.format("Version: %s", getAttr(mainAttr, "Version")))
+System.out.println(String.format("Author: %s", getAttr(mainAttr, "Author")))
+System.out.println(String.format("Category: %s", category))
